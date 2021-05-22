0694247c2b34a3f306b3257a846ef4ab3c8af07f
StevenAlexander44 <64290624+StevenAlexander44@users.noreply.github.com>
2021-05-21 23:26:11 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,6 +4,6 @@ apt upgrade -y
 pkg install python -y
 pkg install ffmpeg -y
 pip install youtube-dl
-mkdir -p /data/data/com.termux/files/home/storage/shared/YoutubeDL
+mkdir -p ~/storage/shared/YoutubeDL
 mkdir ~/bin
 curl https://raw.githubusercontent.com/StevenAlexander44/termux-youtube-dl/main/termux-url-opener -o ~/bin/termux-url-opener
