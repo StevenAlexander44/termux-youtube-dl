518f125980cc8f8221a7a443517167088f3ee028
StevenAlexander44 <64290624+StevenAlexander44@users.noreply.github.com>
2021-07-19 02:47:08 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,5 +2,6 @@ termux-setup-storage
 pkg install python -y
 pip install youtube-dl
 pkg install ffmpeg -y
+pkg install termux-api -y
 mkdir ~/bin
 curl https://raw.githubusercontent.com/StevenAlexander44/termux-youtube-dl/main/termux-url-opener -o ~/bin/termux-url-opener
