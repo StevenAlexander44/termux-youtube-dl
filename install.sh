227376f8191e5fbf12efad9e73d38197d019516f
StevenAlexander44 <64290624+StevenAlexander44@users.noreply.github.com>
2021-07-25 11:40:48 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 termux-setup-storage
 pkg install python -y
 pip install youtube-dl
-pkg install ffmpeg -y
-pkg install termux-api -y
+pkg install jq -y
+echo You have the choice to install the termux-api package, but it wont work without the app
+pkg install termux-api
 mkdir ~/bin
 curl https://raw.githubusercontent.com/StevenAlexander44/termux-youtube-dl/main/termux-url-opener -o ~/bin/termux-url-opener
