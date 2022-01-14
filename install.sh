6f0f5999ff0fada0536fda19de1fa51f37e2e1cc
StevenAlexander44 <64290624+StevenAlexander44@users.noreply.github.com>
2022-01-13 22:03:43 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
-termux-setup-storage
+termux-setup-storage; echo y
 pkg install python termux-api jq --no-install-recommends -y
 pip install yt-dlp
 mkdir ~/bin
 cd ~/bin
+mkdir -p ~/.config/yt-dlp
+echo -e '-o ~/storage/downloads' > ~.config/yt-dlp/config
 curl -LO https://raw.githubusercontent.com/StevenAlexander44/termux-youtube-dl/main/termux-url-opener
